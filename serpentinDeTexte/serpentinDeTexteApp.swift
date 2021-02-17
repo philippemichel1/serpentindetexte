233f681eb5c1f245f9b997507eff3e0545e8e434
Philippe MICHEL <philippe.michel1@gmail.com>
2021-02-17 14:18:37 +0100

    Initial Commit

---
+++ serpentinDeTexte/serpentinDeTexteApp.swift
@@ -0,0 +1,17 @@
+//
+//  serpentinDeTexteApp.swift
+//  serpentinDeTexte
+//
+//  Created by Philippe MICHEL on 17/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct serpentinDeTexteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
