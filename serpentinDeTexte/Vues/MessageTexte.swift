d27c78ce79cb4c9d6345766fe7d43961a9740ed9
Philippe MICHEL <philippe.michel1@gmail.com>
2021-02-17 23:13:58 +0100

    Création vue about

--- serpentinDeTexte/Vues/MessageTexte.swift
+++ serpentinDeTexte/Vues/MessageTexte.swift
@@ -26,52 +26,54 @@ struct MessageTexte: View {
                 .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/)
                 .cornerRadius(10)
             Spacer()
-            //champs de saisie TextField
-            TextField("userText", text: $texteSaisie)
-                .padding(10)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .background(Color.gray)
-            //Bouton afficher le serpentin
-            Button("serpentineButton", action:verificationSaisie)
-                .padding()
-                .foregroundColor(.red)
-                .background(Color.gray)
-                .cornerRadius(10)
-            NavigationLink(
-                destination: Serpentin(chaineSaisie: $texteSaisie),
-                isActive:$champsDeSaisieRempli,
-                label: {
-                    EmptyView()
-                })
-                //affiche une alerte si le champs de saisie est vide
-                .alert(isPresented: $montreAlerte, content: {
-                    Alert(title: Text("alert"))
-                })
-//            // montrer genetre A propos xx
-//            if montrerFenetreAPropos {
-//                ZStack(alignment: .center) {
-//                    FenetreMenu(largeurFenetre: 270, hauteurFenetre: 130)
-//                    VStack {
-//                        HStack {
-//                            Image(Ressources.images.titastus.rawValue)
-//                                .resizable()
-//                                .frame(width: 50, height: 50, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-//                                .aspectRatio(contentMode: .fit)
-//                                .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
-//                            Button(action: {
-//                                montrerSafari.toggle()
-//                            }, label: {
-//                                Text("www.titastus.com")
-//                            }) .sheet(isPresented: $montrerSafari) {
-//                                ControleurSafari(url: URL(string: self.urlString)!)
-//                                .padding()
-//                            }
-//                        }
-//                        
-//                        BoutonFermeture(fermetureFenetre: $montrerFenetreAPropos, couleurFond: .black, couleurTexte: .red)
-//                    }
-//                }
-//            } // fin de if 
+                //champs de saisie TextField
+                TextField("userText", text: $texteSaisie)
+                    .padding(10)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .background(Color.gray)
+                
+                //Bouton afficher le serpentin
+                Button("serpentineButton", action:verificationSaisie)
+                    .padding()
+                    .foregroundColor(.red)
+                    .background(Color.gray)
+                    .cornerRadius(10)
+                NavigationLink(
+                    destination: Serpentin(chaineSaisie: $texteSaisie),
+                    isActive:$champsDeSaisieRempli,
+                    label: {
+                        EmptyView()
+                    })
+                    //affiche une alerte si le champs de saisie est vide
+                    .alert(isPresented: $montreAlerte, content: {
+                        Alert(title: Text("alert"))
+                    })// fin bouton
+            
+            // montrer genetre A propos
+            if montrerFenetreAPropos {
+                ZStack(alignment: .center) {
+                    FenetreMenu(largeurFenetre: 270, hauteurFenetre: 130)
+                    VStack {
+                        HStack {
+                            Image(Ressources.images.titastus.rawValue)
+                                .resizable()
+                                .frame(width: 50, height: 50, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                                .aspectRatio(contentMode: .fit)
+                                .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                            Button(action: {
+                                montrerSafari.toggle()
+                            }, label: {
+                                Text("www.titastus.com")
+                            }) .sheet(isPresented: $montrerSafari) {
+                                ControleurSafari(url: URL(string: self.urlString)!)
+                                    .padding()
+                            }
+                        }
+                        
+                        BoutonFermeture(fermetureFenetre: $montrerFenetreAPropos, couleurFond: .black, couleurTexte: .red)
+                    }
+                }
+            } // fin de if
             Spacer()
                 .toolbar {
                     ToolbarItem(placement: .bottomBar) {
@@ -94,11 +96,8 @@ struct MessageTexte: View {
                             Spacer()
                             
                         }
-                        
                     }
                 }
-            //
-            
         }
     }
     //Vérification du textField il ne doit pas etre vide.
